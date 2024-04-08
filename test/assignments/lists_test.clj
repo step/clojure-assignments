48b6f7e7f7716e8440bca912baeb4abbcccf52e8
Srijayanth Sridhar <131062+craftybones@users.noreply.github.com>
2024-04-08 16:12:20 +0530

    Consolidating old lists and new sequences assignments

--- test/assignments/lists_test.clj
+++ test/assignments/lists_test.clj
@@ -127,3 +127,52 @@
     (are [x y] (= x y)
       [4 5 6] (max-three-digit-sequence [1 2 4 5 6])
       [5 2 4] (max-three-digit-sequence [1 3 5 2 4 1]))))
+
+(deftest ^:kaocha/pending max-of-pairs-test
+  (testing "max of pairs with 0 elements"
+    (is (= [] (max-of-pairs []))))
+  (testing "max of pairs with 1 element"
+    (is (= [1] (max-of-pairs [1]))))
+  (testing "max of pairs with 2 elements"
+    (is (= [2] (max-of-pairs [1 2]))))
+  (testing "max of pairs with more than 2 elements"
+    (are [x y] (= x (max-of-pairs y))
+      [2 3] [1 2 3]
+      [2 3 4] [1 2 3 4]
+      [2 3 4 5] [1 2 3 4 5]
+      [3 3] [3 3 3]
+      [2 2 3] [1 2 1 3])))
+
+(deftest ^:kaocha/pending filter-by-index-test
+  (testing "filter-by-index returns elements at odd indices"
+    (are [result pred coll] (= result (filter-by-index pred coll))
+      [] odd? []
+      [] odd? [1]
+      [2] odd? [1 2 3]
+      [2 4] odd? [1 2 3 4]
+      [2 4] odd? [1 2 3 4 5]))
+  (testing "filter-by-index returns elements at even indices"
+    (are [result pred coll] (= result (filter-by-index pred coll))
+      [] even? []
+      [1] even? [1]
+      [1 3] even? [1 2 3]
+      [1 3] even? [1 2 3 4]
+      [1 3 5] even? [1 2 3 4 5]))
+  (testing "filter-by-index returns elements at custom indices"
+    (are [result pred coll] (= result (filter-by-index pred coll))
+      [] #{0 1 4 5} []
+      [1] #{0 1 4 5} [1]
+      [1 2] #{0 1 4 5} [1 2]
+      [1 2] #{0 1 4 5} [1 2 3]
+      [1 2] #{0 1 4 5} [1 2 3 4]
+      [1 2 5] #{0 1 4 5} [1 2 3 4 5]
+      [1 2 5 6] #{0 1 4 5} [1 2 3 4 5 6])))
+
+(deftest ^:kaocha/pending collatz-sequence-test
+  (testing "collatz sequences"
+    (are [x y] (= (collatz-sequence x) y)
+      1 [1]
+      2 [2 1]
+      3 [3 10 5 16 8 4 2 1]
+      20 [20 10 5 16 8 4 2 1]
+      23 [23 70 35 106 53 160 80 40 20 10 5 16 8 4 2 1])))
