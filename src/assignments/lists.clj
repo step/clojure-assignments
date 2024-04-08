d6344262c74b514cdf21caf78e30cadca9eec920
Srijayanth Sridhar <131062+craftybones@users.noreply.github.com>
2024-04-08 16:07:06 +0530

    Adding older tests from a few years ago

---
+++ src/assignments/lists.clj
@@ -0,0 +1,226 @@
+(ns assignments.lists)
+
+(defn map'
+  "Implement a non-lazy version of map that accepts a
+  mapper function and several collections. The output
+  should be consistent with clojure.core/map"
+  {:level        :medium
+   :use          '[loop recur]
+   :dont-use     '[map]}
+  [f & colls])
+
+(defn filter'
+  "Implement a non-lazy version of filter that accepts a
+  predicate function and a collection. The output
+  should be consistent with clojure.core/filter"
+  {:level        :easy
+   :use          '[loop recur]
+   :dont-use     '[filter]}
+  [pred coll])
+
+(defn reduce'
+  "Implement your own multi-arity version of reduce
+  that accepts a predicate function and a collection.
+  The output should be consistent with clojure.core/reduce"
+  {:level        :medium
+   :use          '[loop recur]
+   :dont-use     '[reduce]}
+  ([f coll])
+  ([f init coll]))
+
+(defn count'
+  "Implement your own version of count that counts the
+  number of elements in a given sequence"
+  {:level        :easy
+   :use          '[loop recur]
+   :dont-use     '[count]}
+  [coll])
+
+(defn reverse'
+  "Implement your own version of reverse that reverses a coll.
+  Returns nil if coll provided is not a sequence"
+  {:level        :easy
+   :use          '[reduce conj seqable? when]
+   :dont-use     '[reverse]}
+  [coll])
+
+(defn every?'
+  "Implement your own version of every? that checks if every
+  element of a coll satisfies the given predicate"
+  {:level        :easy
+   :use          '[loop recur and]
+   :dont-use     '[every?]}
+  [pred coll])
+
+(defn some?'
+  "Implement your own version of some that checks if at least one
+  element of a coll satisfies the given predicate. Always return
+  a boolean. The original clojure.core/some returns a nil when
+  no element satisfies the given predicate"
+  {:level        :easy
+   :use          '[loop recur or]
+   :dont-use     '[some]}
+  [pred coll])
+
+(defn ascending?
+  "Verify if every element is greater than or equal to its predecessor"
+  {:level        :easy
+   :use          '[partition every? partial apply <=]
+   :dont-use     '[loop recur]}
+  [coll])
+
+(defn distinct'
+  "Implement your own lazy sequence version of distinct which returns
+  a collection with duplicates eliminated. Might have to implement another
+  function, or use a letfn"
+  {:level        :medium
+   :use          '[lazy-seq set conj let :optionally letfn]
+   :dont-use     '[loop recur distinct]}
+  [coll])
+
+(defn dedupe'
+  "Implement your own lazy sequence version of dedupe which returns
+  a collection with consecutive duplicates eliminated (like the uniq command).
+  Might have to implement another function, or use a letfn"
+  {:level        :medium
+   :use          '[lazy-seq conj let :optionally letfn]
+   :dont-use     '[loop recur dedupe]}
+  [coll])
+
+(defn sum-of-adjacent-digits
+  "Given a collection, returns a map of the sum of adjacent digits.
+  [a b c] => [a+b b+c]"
+  {:level        :medium
+   :use          '[map + rest]
+   :dont-use     '[loop recur partition]}
+  [coll])
+
+(defn max-three-digit-sequence
+  "Given a collection of numbers, find a three digit sequence that
+  yields the max sum. If the collection has fewer than 3 elements,
+  returns the collection itself.
+  [1 2 -1 2 0] => [2 -1 2]"
+  {:level        :medium
+   :use          '[map next nnext max-key partial apply + if ->>]
+   :dont-use     '[loop recur partition]}
+  [coll])
+
+;; transpose is a def. Not a defn.
+(def
+  ^{:level        :easy
+    :dont-use     '[loop recur for nth get]}
+  transpose
+  "Transposes a given matrix.
+  [[a b] [c d]] => [[a c] [b d]].
+  Note this is a def. Not a defn.
+  Return a vector of vectors, not list of vectors or vectors of lists"
+  (constantly nil))
+
+(defn difference
+  "Given two collections, returns only the elements that are present
+  in the second coll but not the first"
+  {:level        :easy
+   :use          '[remove set]
+   :dont-use     '[loop recur if]}
+  [coll1 coll2])
+
+(defn union
+  "Given two collections, returns a new collection with elements from the second
+  collection added to the first collection if they are missing in the first
+  collection to begin with. Return a list, not a set. It also doesn't matter
+  if elements repeat."
+  {:level        :easy
+   :use          '[remove into set ->>]}
+  [coll1 coll2])
+
+;; points-around-origin is a def not a defn
+(def
+  ^{:level        :easy
+    :use          '[for]
+    :dont-use     '[hardcoded-values map filter]}
+  points-around-origin
+  "Calculate all the points around the origin
+  [-1 -1] [0 -1] [1 -1] etc. There should be 8 points
+  Note this is a def, not a defn")
+
+(defn cross-product
+  "Given two sequences, generate every combination in the sequence
+  until two elements are equal
+  [1 2 3] [4 3 5] =>
+  [[1 4] [1 3] [1 5] [2 4] [2 3] [2 5] [3 4]]"
+  {:level        :easy
+   :use          '[for]}
+  [seq1 seq2])
+
+(defn double-up
+  "Given a collection, return a new collection that contains
+  each element repeated twice"
+  {:level        :easy
+   :use          '[mapcat partial repeat :optionally vector]}
+  [coll])
+
+(defn third-or-fifth
+  "Given a collection return a new collection that contains
+  elements whose index is either divisible by three or five"
+  {:level        :easy
+   :use          '[keep-indexed when :optionally map-indexed filter]}
+  [coll])
+
+(defn sqr-of-the-first
+  "Given a collection, return a new collection that contains the
+  same number of elements as the given collection all of which
+  are the square of the first element
+  [4 5 6] => [16 16 16]"
+  {:level        :easy
+   :use          '[map constantly let]}
+  [coll])
+
+(defn russian-dolls
+  "Given a collection and a number, wrap each element in a nested vector
+  with a nesting factor of the number provided.
+  [1 2 3] 3 => [[[1]] [[2]] [[3]]]"
+  {:level        :medium
+   :use          '[iterate mapv partial vector drop first ->>]
+   :dont-use     '[for loop recur reduce]}
+  [coll nesting-factor])
+
+(defn split-comb
+  "Given a collection, return a new sequence where the first
+  half of the sequence is interleaved with the second half.
+  If the given collection has an odd number of elements, then
+  preserve the last element of the original collection
+  [1 2 3 4 5] => [1 3 2 4 5]"
+  {:level        :easy
+   :use          '[interleave split-at if rem concat take-last]
+   :dont-use     '[loop recur map-indexed take drop]}
+  [coll])
+
+(defn muted-thirds
+  "Given a sequence of numbers, make every third element
+  0 while preserving the other elements
+  [1 2 8 4 15 2 7] => [1 2 0 4 15 0 7]"
+  {:level        :easy
+   :use          '[map cycle]
+   :dont-use     '[loop recur map-indexed take take-nth]}
+  [coll])
+
+(defn palindrome?
+  "Implement a recursive palindrome check of any given sequence"
+  {:level        :easy
+   :use          '[empty? loop recur butlast rest]
+   :dont-use     '[reverse]}
+  [coll])
+
+(defn index-of
+  "index-of takes a sequence and an element and finds the index
+  of the element in the given sequence. Returns -1 if element
+  is not found"
+  {:level        :easy
+   :use          '[loop recur rest]
+   :dont-use     '[.indexOf memfn]}
+  [coll n])
+
+(defn validate-sudoku-grid
+  "Given a 9 by 9 sudoku grid, validate it."
+  {:level        :hard}
+  [grid])
---
+++ test/assignments/lists_test.clj
@@ -0,0 +1,129 @@
+(ns assignments.lists-test
+  (:require [clojure.test :refer :all]
+            [assignments.lists :refer :all :as l]))
+
+(deftest ^:kaocha/pending map-test
+  (testing "identity with single coll"
+    (is (= [1 2 3] (map' identity [1 2 3])))))
+
+(deftest ^:kaocha/pending filter-test
+  (testing "even? predicate"
+    (is (= [2 4 6] (filter' even? [1 2 3 4 5 6 9])))))
+
+(deftest ^:kaocha/pending reduce-test
+  (testing "arity 2"
+    (is (= 15 (reduce' + [1 2 3 4 5]))))
+  (testing "arity 3"
+    (is (= 15 (reduce' + 1 [2 3 4 5])))))
+
+(deftest ^:kaocha/pending count-test
+  (testing "sequence length"
+    (is (= 5 (count' [1 2 3 4 5]))))
+  (testing "nil"
+    (is (= 0 (count' nil))))
+  (testing "[]"
+    (is (= 0 (count' []))))
+  (testing "map"
+    (is (= 2 (count' {:one 1 :two 2}))))
+  (testing "string"
+    (is (= 6 (count' "abcdef")))))
+
+(deftest ^:kaocha/pending reverse-test
+  (testing "seqable collection"
+    (is (= [5 4 3 2 1] (reverse' [1 2 3 4 5]))))
+  (testing "non-seqable collection"
+    (is (nil? (reverse' 1)))))
+
+(deftest ^:kaocha/pending every-test
+  (testing "true condition"
+    (is (true? (every?' even? [2 4 6 8]))))
+  (testing "false condition"
+    (is (false? (every?' even? [2 4 9 6 8])))))
+
+(deftest ^:kaocha/pending some-test
+  (testing "true condition"
+    (is (true? (some?' even? [1 5 3 4]))))
+  (testing "false condition"
+    (is (false? (some?' even? [1 5 7 9])))))
+
+(deftest ^:kaocha/pending ascending-test
+  (testing "true condition"
+    (is (true? (ascending? [1 2 3 4 5 6]))))
+  (testing "false condition"
+    (is (false? (ascending? [1 2 3 2 1 0])))))
+
+(deftest ^:kaocha/pending index-of-test
+  (testing "element present in collection"
+    (is (= 4 (index-of [1 2 3 4 5 6] 5))))
+  (testing "element not-present in collection"
+    (is (= -1 (index-of [1 2 3 4 5 6] 9)))))
+
+(deftest ^:kaocha/pending palindrome?-test
+  (testing "true condition"
+    (is (true? (palindrome? ["n" "a" "m" "a" "n"]))))
+  (testing "false condition"
+    (is (false? (palindrome? ["k" "a" "n" "n" "u"])))))
+
+(deftest ^:kaocha/pending sqr-of-the-first-test
+  (testing "non-empty collection"
+    (is (= [16 16 16] (sqr-of-the-first [4 5 6])))))
+
+(deftest ^:kaocha/pending double-up-test
+  (testing "single dimensional collection"
+    (is (= [1 1 2 2 3 3] (double-up [1 2 3]))))
+  (testing "two dimensional collection"
+    (is (= [[1 2] [1 2] [3 4] [3 4]] (double-up [[1 2] [3 4]])))))
+
+(deftest ^:kaocha/pending distinct-test
+  (testing "non-empty collection"
+    (is (= [1 2 4 5 3] (distinct' [1 2 1 2 4 5 3 5])))))
+
+(deftest ^:kaocha/pending dedupe'-test
+  (testing "non-empty collection"
+    (is (= [1 2 1 4 5 3 5] (dedupe' [1 1 2 2 1 4 5 3 5])))))
+
+(deftest ^:kaocha/pending sum-of-adjacent-digits-test
+  (testing "sum of adjacent digits of [1 2 3]"
+    (is (= [3 5] (sum-of-adjacent-digits [1 2 3])))))
+
+(deftest ^:kaocha/pending difference-test
+  (testing "non-empty collection"
+    (is (= [2 3] (difference [1 2 3] [1])))))
+
+(deftest ^:kaocha/pending union-test
+  (testing "non-empty collection"
+    (is (= [1 2 3] (union [1 2] [2 3])))))
+
+(deftest ^:kaocha/pending transpose-test
+  (testing "non-empty collection"
+    (is (= [[:a :d] [:b :e] [:c :f]] (transpose [[:a :b :c] [:d :e :f]])))))
+
+(deftest ^:kaocha/pending cross-product-test
+  (testing "non-empty collection"
+    (is (= [[1 4] [1 3] [1 5] [2 4] [2 3] [2 5] [3 4]] (cross-product [1 2 3] [4 3 5])))))
+
+(deftest ^:kaocha/pending third-or-fifth-test
+  (testing "non-empty collection"
+    (is (= [1 4] (third-or-fifth [1 2 3 4])))))
+
+(deftest ^:kaocha/pending russian-dolls-test
+  (testing "[1 2 3] 3 => [[[1]] [[2]] [[3]]]"
+    (is (= [[[1]] [[2]] [[3]]]  (russian-dolls [1 2 3] 3)))))
+
+(deftest ^:kaocha/pending muted-thirds-test
+  (testing "[1 2 8 4 15 2 7] => [1 2 0 4 15 0 7]"
+    (is (= [1 2 0 4 15 0 7] (muted-thirds [1 2 8 4 15 2 7])))))
+
+(deftest ^:kaocha/pending split-comb-test
+  (testing "odd length collection"
+    (is (= '(1 3 2 4 5) (split-comb [1 2 3 4 5]))))
+  (testing "even length collection"
+    (is (= '(1 4 2 5 3 6) (split-comb [1 2 3 4 5 6])))))
+
+(deftest ^:kaocha/pending max-three-digit-sequence-test
+  (testing "less than 3 elements"
+    (is (= [1 2] (max-three-digit-sequence [1 2]))))
+  (testing "three elements or more"
+    (are [x y] (= x y)
+      [4 5 6] (max-three-digit-sequence [1 2 4 5 6])
+      [5 2 4] (max-three-digit-sequence [1 3 5 2 4 1]))))
